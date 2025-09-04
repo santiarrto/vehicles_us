eea049705d947e616189dc4bd70a36df6451934b
Santiago Arredondo <santiarrto@gmail.com>
2025-09-04 14:48:13 -0500

    Updating filepath on app

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-car_data = pd.read_csv('vehicles_us/vehicles_us.csv')
+car_data = pd.read_csv('vehicles_us.csv')
 
 
 hist_checkbox = st.checkbox('Construir histograma') # crear un botón
